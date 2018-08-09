1a5e6214f3618f03abd524259bc420dc914b8742
HsingYin <582136509@qq.com>
2018-08-09 11:49:53 +0800

    样式调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,19 @@
 <template>
     <el-card shadow="always" class="post">
         <div slot="header" class="clearfix">
-            <el-button style="float: right; padding: 3px 0" type="text">查看详情</el-button>
-            <img class="avatar" v-bind:src="this.post.author.avatar" alt="avatar">
             <div class="container">
               <div class="post-prview">
-                <a class="post-title" v-bind:href="this.post">{{ this.post.title }}</a>
+                <img class="avatar" v-bind:src="this.post.author.avatar" alt="avatar">
+                <a class="post-title" v-bind:href="this.post.url">{{ this.post.title }}</a>
+                <el-badge :value="this.post.replies" :max="999" class="item">
+                  <el-button>回帖</el-button>
+                </el-badge>
+                <a class="author" v-bind:href="this.post">{{ this.post.author.userName }}</a>
               </div>
             </div>
+        </div>
+        <div class="content clearfix">
+          <el-button style="float: right;" type="text" @click="handlePostDetail">查看原帖</el-button>
         </div>
     </el-card>
 </template>
@@ -26,24 +32,30 @@ export default {
           avatar: '',
           url: ''
         },
-        replies: ''
+        replies: 0
       }
     }
   },
   created: function () {
+    this.getList()
+  },
+  methods: {
+    getList () {
     // 获取热门主题下的信息
-    this.$axios.get('api/topics/hot.json')
-      .then((response) => {
-        console.log(response.data)
-        var topics = response.data
-        this.post.title = topics[0].title
-        this.post.url = topics[0].url
-        this.replies = topics[0].replies
-        this.post.author.avatar = topics[0].member.avatar_mini
-      })
-      .catch((error) => {
-        console.log(error)
-      })
+      this.$axios.get('api/topics/hot.json')
+        .then((response) => {
+          console.log(response.data)
+          var topics = response.data
+          this.post.title = topics[0].title
+          this.post.url = topics[0].url
+          this.post.replies = topics[0].replies
+          this.post.author.userName = topics[0].member.username
+          this.post.author.avatar = topics[0].member.avatar_mini
+          // this.changeLargeAvatar(topics[0].member.avatar_mini)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
     // 获取热门回复
     // this.$axios.get('api/replies/show.json', {
     //   params: {
@@ -54,10 +66,14 @@ export default {
     // }).catch((error) => {
     //   console.log(error)
     // })
-  },
-  methods: {
+    },
     handlePostDetail () {
-
+      window.location.href = this.post.url
+    },
+    changeLargeAvatar (url) {
+      console.log('传入的url' + url)
+      let newURL = url.replace('/mini/g', 'large')
+      console.log('新的url' + newURL)
     }
   }
 }
@@ -77,15 +93,43 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: inherit;
+  text-decoration: none;
 }
 .post {
   width: 800px;
   margin: 0 auto;
 }
+.post-prview {
+  height: 50px;
+}
 .avatar {
-  width: 30px;
-  height: 30px;
+  width: 48px;
+  height: 48px;
   float: left;
+}
+.post-title {
+  width: 80%;
+  padding-left: 10px;
+  color: #1a1a1a;
+  text-align: left;
+  font-size: 16px;
+  line-height: 1.6;
+  font-weight: 600;
+  float: left;
+}
+.author {
+  padding-left: 10px;
+  color: black;
+  float: left;
+}
+.item {
+  width: 50px;
+  height: 30px;
+  margin-right: 18px;
+  float: right;
+}
+.el-badge__content {
+  height: 30;
 }
 </style>
